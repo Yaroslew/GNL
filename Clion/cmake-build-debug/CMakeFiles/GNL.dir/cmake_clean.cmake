45eda1498c28f118b8e4e515506c406e94ee2d06
Pcorlys- <Yarosleu@gmail.com>
2018-12-30 14:13:48 +0300

    start new project on Clion

---
+++ Clion/cmake-build-debug/CMakeFiles/GNL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GNL.dir/main.c.o"
+  "GNL.pdb"
+  "GNL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GNL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
