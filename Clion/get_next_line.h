6ce7d9687e5ae91913538a357dea754874882c89
Pcorlys- <Yarosleu@gmail.com>
2019-01-08 13:24:08 +0300

    new files

--- Clion/get_next_line.h
+++ Clion/get_next_line.h
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcorlys- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/08 13:03:56 by pcorlys-          #+#    #+#             */
-/*   Updated: 2019/01/08 13:03:58 by pcorlys-         ###   ########.fr       */
+/*   Created: 2019/01/08 13:04:06 by pcorlys-          #+#    #+#             */
+/*   Updated: 2019/01/08 13:04:07 by pcorlys-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 6
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#define struct		s_list
 {
+	char			*str;
+	size_t			size;
+	int 			fd;
+	struct s_list	*next;
+} t_list;
 
-}
+int get_next_line(const int fd, char **line);
+
+# endif
